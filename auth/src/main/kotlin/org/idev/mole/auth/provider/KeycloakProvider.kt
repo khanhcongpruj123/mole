cb5d375e2bfc401126a034a4a3a3287bc692529f
idev <khanhcongpruj123@gmail.com>
2022-11-22 16:51:20 +0700

    Remove unused code

--- auth/src/main/kotlin/org/idev/mole/auth/provider/KeycloakProvider.kt
+++ auth/src/main/kotlin/org/idev/mole/auth/provider/KeycloakProvider.kt
@@ -1,7 +1,6 @@
 package org.idev.mole.auth.provider
 
 import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder
-import org.keycloak.OAuth2Constants
 import org.keycloak.adapters.springboot.KeycloakSpringBootProperties
 import org.keycloak.admin.client.KeycloakBuilder
 import org.springframework.stereotype.Component
@@ -18,15 +17,4 @@ class KeycloakProvider(private val prop: KeycloakSpringBootProperties) {
         .password(password)
         .resteasyClient(ResteasyClientBuilder().connectionPoolSize(20).build())
         .build()
-
-    fun keycloakWithAccessToken(username: String, password: String) = KeycloakBuilder.builder()
-        .serverUrl(prop.authServerUrl)
-        .realm(prop.realm)
-        .clientId(prop.resource)
-        .clientSecret(prop.credentials["secret"].toString())
-        .grantType(OAuth2Constants.REFRESH_TOKEN)
-        .resteasyClient(ResteasyClientBuilder().connectionPoolSize(20).build())
-        .build()
-
-
 }
