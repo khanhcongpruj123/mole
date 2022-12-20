59f1cc99ff7f84030893664e36f1e3c1a08a23cd
idev <khanhcongpruj123@gmail.com>
2022-12-20 13:44:37 +0700

    Modify model

--- post-service/src/main/kotlin/org/idev/mole/post/models/Post.kt
+++ post-service/src/main/kotlin/org/idev/mole/post/models/Post.kt
@@ -1,18 +1,14 @@
 package org.idev.mole.post.models
 
 import java.util.*
+import javax.persistence.Column
 import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
 import javax.persistence.Table
 
 @Entity
 @Table(name = "tbl_post")
 class Post(
-    @Id
-    @GeneratedValue
-    val id: UUID? = null,
     var userId: UUID,
-    var name: String? = null
-) {
-}
+    @Column(nullable = false)
+    var content: String
+) : BaseModel()
