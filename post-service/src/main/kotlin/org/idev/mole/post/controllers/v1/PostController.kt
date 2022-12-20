42da01e69b98c9fcb9ece79601756de39b926c5a
idev <khanhcongpruj123@gmail.com>
2022-12-20 13:45:17 +0700

    Add validation

--- post-service/src/main/kotlin/org/idev/mole/post/controllers/v1/PostController.kt
+++ post-service/src/main/kotlin/org/idev/mole/post/controllers/v1/PostController.kt
@@ -1,13 +1,15 @@
-package org.idev.mole.post.routers.v1
+package org.idev.mole.post.controllers.v1
 
 import org.idev.mole.post.dtos.PostDTO
 import org.idev.mole.post.mappers.PostMapper
 import org.idev.mole.post.services.PostService
 import org.json.JSONObject
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 import java.util.*
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -16,15 +18,21 @@ class PostController(
     val postMapper: PostMapper
 ) {
 
-    @GetMapping
+    @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
     fun posts(@AuthenticationPrincipal session: JSONObject): ResponseEntity<List<PostDTO>> {
         val userId = session.getJSONObject("identity")
             .getString("id")
         return ResponseEntity.ok(postService.postRepository.findByUserId(UUID.fromString(userId)).map { postMapper.map(it) })
     }
 
-    @PostMapping
-    fun createPost(@AuthenticationPrincipal session: JSONObject, @RequestBody postDTO: PostDTO): ResponseEntity<Any> {
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE])
+    fun createPost(
+        @AuthenticationPrincipal
+        session: JSONObject,
+        @Valid
+        @RequestBody
+        postDTO: PostDTO
+    ): ResponseEntity<Any> {
         val userId = session.getJSONObject("identity")
             .getString("id")
         postDTO.userId = userId
--- post-service/src/main/kotlin/org/idev/mole/post/dtos/PostDTO.kt
+++ post-service/src/main/kotlin/org/idev/mole/post/dtos/PostDTO.kt
@@ -1,8 +1,10 @@
 package org.idev.mole.post.dtos
 
 import java.io.Serializable
+import javax.validation.constraints.NotNull
 
 data class PostDTO(
     var userId: String? = null,
-    var name: String? = null
+    @NotNull
+    val content: String? = null,
 ) : Serializable
