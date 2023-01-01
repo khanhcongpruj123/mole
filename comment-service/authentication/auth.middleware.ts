b24aee484ddf75e1fb86513dec5f6fe7930fbd6c
idev <khanhcongpruj123@gmail.com>
2023-01-01 11:16:37 +0700

    Add comment

--- comment-service/authentication/auth.middleware.ts
+++ comment-service/authentication/auth.middleware.ts
@@ -2,6 +2,11 @@ import { NextFunction, RequestHandler, Response, Request } from "express";
 import { AuthRequest } from "@auth/auth.type";
 import * as kratosService from "../kratos/kratos.service";
 
+/**
+ * Auth middleware will get bearer token and check it by "whoAmI" Kratos API
+ * Then, set auth session is kratos auth session
+ * User can get user id, session id,... by auth session
+ */
 const authMiddleware = async (
   req: AuthRequest,
   res: Response,
--- comment-service/kratos/kratos.service.ts
+++ comment-service/kratos/kratos.service.ts
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+/**
+ * Get auth kratos auth session
+ */
 export const whoAmI = (token: string) => {
   return axios.get("http://127.0.0.1:4433/sessions/whoami", {
     headers: {
